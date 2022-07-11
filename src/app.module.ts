f24d4f20085a65b5499930f3149e0e9f3079ee5c
Kimihiko Sasaki <tappurin555@gmail.com>
2022-07-11 17:36:45 +0900

    起動時、データベースに繋げないエラーへの対処

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,21 @@ import { TaskModule } from './task/task.module';
 //Moduleは、ControllerやServiceの依存関係を管理します。moduleファイルに対して、ControllerやServiceを登録することで、そのControllerやServiceが使えるようになります。
 //Appモジュールがルートとなり、下位に各機能のモジュールが存在するイメージです。
 @Module({
-  imports: [TypeOrmModule.forRoot(), TaskModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'radwimps123',
+      database: 'todolist',
+      entities: ['dist/entities/**/*.entity.js'],
+      migrations: ['dist/migrations/**/*.js'],
+      migrationsTableName: 'custom_migration_table',
+      logging: false,
+    }),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
